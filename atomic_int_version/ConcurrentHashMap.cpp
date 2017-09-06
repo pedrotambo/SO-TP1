64f01af5e74406568af1672dcc0382ebe3ebab13
Pedro Tamborindeguy <pftambo@gmail.com>
2017-09-06 04:20:35 -0300

    Versión atomic int funcionando sin trabar mutex en addinc (en carpeta aparte)

--- atomic_int_version/ConcurrentHashMap.cpp
+++ atomic_int_version/ConcurrentHashMap.cpp
@@ -83,9 +83,7 @@ void ConcurrentHashMap::addAndInc(string key){
 	/* Incrementamos su valor */
 	for (auto it = tabla[h]->CrearIt(); it.HaySiguiente(); it.Avanzar()) {
 		if (it.Siguiente().first == key) {
-			pthread_mutex_lock(& _locks[h]);
 			it.Siguiente().second++;
-			pthread_mutex_unlock(& _locks[h]);
 			break;
 		}
 	}
